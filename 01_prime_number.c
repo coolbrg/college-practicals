681980d730a0795d97d7f30494f36db056c11702
BRG <brgclasses@gmail.com>
2021-02-27 12:07:07 +0530

    Done practical upto even or odd

---
+++ 01_prime_number.c
@@ -0,0 +1,6 @@
+// Program to find prime or not
+
+#include<stdio.h>
+int main(){
+  // program
+}
---
+++ 02_even_or_odd.c
@@ -0,0 +1,7 @@
+// Program to find even or odd
+
+#include <stdio.h>
+int main()
+{
+  // program for even odd
+}
